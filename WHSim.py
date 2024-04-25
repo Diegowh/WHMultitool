158a3c754a144fd7ae00fd52975a5742ab304ec4
diegowh <contacto.diegowh@gmail.com>
2024-04-25 18:26:45 +0200

    First functional version

--- WHSim.py
+++ WHSim.py
@@ -1,4 +1,5 @@
-from threading import Thread, Event
+import asyncio
+import threading
 import time
 import tkinter as tk
 import keyboard
@@ -9,8 +10,10 @@ class AutoSim(tk.Tk):
     def __init__(self):
         super().__init__()
         
-        self.autosim_running = False
-        self.autosim_thread = None
+        self.autosim_task = None
+        self.loop = asyncio.new_event_loop()
+        self.thread = threading.Thread(target=self.start_loop, args=(self.loop,))
+        self.thread.start()
         self.ignore_next_stop = False  # error with the code duel hitting the toggle for some reason
         self.autosim_last_click_time = time.time() - 1
         self.autosim_running = False  # This will track the running state internally, not in config
@@ -37,25 +40,34 @@ class AutoSim(tk.Tk):
         map_num_entry = tk.Entry(self, textvariable=self.text_input)
         map_num_entry.pack(padx=10, pady=10)
 
+    def start_loop(self, loop: asyncio.AbstractEventLoop):
+        asyncio.set_event_loop(loop)
+        loop.run_forever()
+        
     def start_autosim(self):
         if not self.autosim_running:
             self.autosim_running = True
-            self.autosim_thread = Thread(target=self.run_auto_sim)
-            self.autosim_thread.start()
+            self.loop.call_soon_threadsafe(self._start_autosim_coroutine)
             print("Auto-sim started")
+
+    def _start_autosim_coroutine(self):
+        self.autosim_task = asyncio.create_task(self.run_auto_sim())
 
     def stop_autosim(self):
         if self.autosim_running:
             self.autosim_running = False
-            if self.autosim_thread is not None:
-                self.autosim_thread.join()
+            if self.autosim_task is not None:
+                self.loop.call_soon_threadsafe(self._stop_autosim_coroutine)
             print("Auto-sim stopped")
+
+    def _stop_autosim_coroutine(self):
+        self.autosim_task.cancel()
     
-    def run_auto_sim(self):
+    async def run_auto_sim(self):
         try:
             while self.autosim_running:
                 map_number = self.text_input.get()
-                self.autosim_routine(map_number)
+                await self.autosim_routine(map_number)
             print("Auto-sim stopping")
         except Exception as e:
             print(f"Error: {e}")
@@ -71,7 +83,7 @@ class AutoSim(tk.Tk):
         width, height = pyautogui.size()
         return width, height
     
-    def move_and_click(self, x, y, sleep_time=0.5):
+    async def move_and_click(self, x, y, sleep_time=0.5):
         screen_w, screen_h = self.get_screen_resolution()
         # Default values are for 1920x1080 resolution, so we need to check if the resolution is different
         if screen_w != 1920 or screen_h != 1080:
@@ -79,29 +91,29 @@ class AutoSim(tk.Tk):
             y *= screen_h / 1080
         pyautogui.moveTo(x, y)
         pyautogui.click()
-        time.sleep(sleep_time)
+        await asyncio.sleep(sleep_time)
     
     # def debug_function(self, map_number):
     #     while self.autosim_running:
     #         print(f"function running with map number: {map_number}")
     #         time.sleep(1)
         
-    def autosim_routine(self, map_number):
+    async def autosim_routine(self, map_number):
         while self.autosim_running:
-            self.move_and_click(934, 858)  # Press middle button on the start screen
-            self.move_and_click(705, 523)  # Press Join Game button
-            self.move_and_click(1670, 200)  # Click on the map search bar
+            await self.move_and_click(934, 858)  # Press middle button on the start screen
+            await self.move_and_click(705, 523)  # Press Join Game button
+            await self.move_and_click(1670, 200)  # Click on the map search bar
             pyautogui.write(map_number)
             pyautogui.press('enter')
-            time.sleep(0.5)  # Wait for the map list to load
-            self.move_and_click(930, 335)  # Click on the first map in the list
-            self.move_and_click(1700, 945, 3)  # Click Join and wait 3 seconds for the mod selection screen to load
-            self.move_and_click(340, 930)  # Click Join
-            time.sleep(10)  # Delay to allow the Server full message to appear
+            await asyncio.sleep(0.5)  # Wait for the map list to load
+            await self.move_and_click(930, 335)  # Click on the first map in the list
+            await self.move_and_click(1700, 945, 3)  # Click Join and wait 3 seconds for the mod selection screen to load
+            await self.move_and_click(340, 930)  # Click Join
+            await asyncio.sleep(10)  # Delay to allow the Server full message to appear
             pyautogui.press('esc')
-            time.sleep(0.5)
-            self.move_and_click(170, 880)
-            self.move_and_click(964, 964)
+            await asyncio.sleep(0.5)
+            await self.move_and_click(170, 880)
+            await self.move_and_click(964, 964)
             
 app = AutoSim()
 app.mainloop()
