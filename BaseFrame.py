26b908896c2f9d925403f1496a7521933b8fa348
diegowh <contacto.diegowh@gmail.com>
2024-04-30 12:43:56 +0200

    Create BaseFrame.py

---
+++ BaseFrame.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from abc import ABC, abstractmethod
+
+
+class BaseFrame(tk.Frame, ABC):
+    def __init__(self, master=None, controller=None):
+        super().__init__(master)
+        self.controller = controller
+        self.create_widgets()
+
+    @abstractmethod
+    def destroy_screen(self):
+        pass
+    
+    def create_widgets(self):
+        back_btn = tk.Button(self, text="Back", command=self.destroy_screen)
+        back_btn.pack(pady=10)
