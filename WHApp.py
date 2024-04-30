c29cc5fedc6ac6becc987e5d2e4fa77b34bdc0b1
diegowh <contacto.diegowh@gmail.com>
2024-04-30 12:44:57 +0200

    Big App architecture refactor

--- WHApp.py
+++ WHApp.py
@@ -1,50 +1,61 @@
 import tkinter as tk
+from config import Config
+from AutoSim import AutoSimGUI
+from BreederManager import BreederManagerGUI
 
-class MainScreen(tk.Frame):
-    def __init__(self, master=None, controller=None):
-        super().__init__(master)
-        self.controller = controller
-        self.create_widgets()
-
-    def create_widgets(self):
-        options = ["Option 1", "Option 2", "Option 3"]
-        for i, option in enumerate(options):
-            btn = tk.Button(self, text=option, command=lambda i=i: self.controller.show_option(i))
-            btn.pack(pady=10)
-
-class OptionScreen(tk.Frame):
-    def __init__(self, master=None, controller=None, option=None):
-        super().__init__(master)
-        self.controller = controller
-        self.option = option
-        self.create_widgets()
-
-    def create_widgets(self):
-        label = tk.Label(self, text=f"{self.option} chosen")
-        label.pack(pady=10)
-
-        back_btn = tk.Button(self, text="Back", command=self.controller.show_main)
-        back_btn.pack(pady=10)
 
 class AppController(tk.Tk):
-    def __init__(self):
+    def __init__(self, config: Config):
         super().__init__()
-        self.geometry(f"250x200")
+        self.config = config
+        
+        self.apps = {
+            "AutoSim": AutoSimGUI,
+            "BreederManager": BreederManagerGUI,
+        }
+        
+        self.title(self.config.APP_TITLE)
+        self.geometry(f"{self.config.APP_WEIGHT}x{self.config.APP_HEIGHT}")
+        self.resizable(False, False)
 
         self.main_screen = MainScreen(master=self, controller=self)
-        self.option_screens = [OptionScreen(master=self, controller=self, option=f"Option {i+1}") for i in range(3)]
-
-        self.show_main()
+        self.main_screen.pack(fill=tk.BOTH, expand=True)
 
     def show_option(self, i):
         self.main_screen.pack_forget()
-        self.option_screens[i].pack(fill=tk.BOTH, expand=True)
+        app_name = list(self.apps.keys())[i]
+        app_class = self.apps[app_name]
+        self.current_option_screen = app_class(config=self.config, master=self, controller=self)
+        self.current_option_screen.pack(fill=tk.BOTH, expand=True)
 
     def show_main(self):
-        for screen in self.option_screens:
-            screen.pack_forget()
         self.main_screen.pack(fill=tk.BOTH, expand=True)
+        del self.current_option_screen
+
+
+class MainScreen(tk.Frame):
+    def __init__(self, master=None, controller: AppController=None):
+        super().__init__(master)
+        self.controller = controller
+        self.create_widgets()
+
+    def create_widgets(self):
+        options = list(self.controller.apps.keys())
+        for i, option in enumerate(options):
+            btn = tk.Button(self, text=option, command=self.create_command(i))
+            btn.pack(pady=10)
+            
+    def create_command(self, i):
+        def command():
+            app_name = list(self.controller.apps.keys())[i]
+            app_class = self.controller.apps[app_name]
+            app_instance = app_class(config=self.controller.config, master=self.controller, controller=self.controller)
+            self.controller.show_option(i)
+        return command
+
+
 
 if __name__ == "__main__":
-    app = AppController()
+    config = Config()
+    app = AppController(config=config)
     app.mainloop()
