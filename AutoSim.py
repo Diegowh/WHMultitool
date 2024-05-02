5dba3c87b623cd3537673215bb8439d747456a4d
diegowh <contacto.diegowh@gmail.com>
2024-05-02 19:26:03 +0200

    Refactor AutoSim to implement BaseTaskManager

--- AutoSim.py
+++ AutoSim.py
@@ -7,100 +7,52 @@ import time
 
 from config import Config
 from BaseFrame import BaseFrame
+from BaseTaskManager import BaseTaskManager
+from utils import get_screen_resolution
+import player_actions as pa
+from screen_manager import (
+    ScreenCoordsEnum,
+    ModsSelectionScreenCoordinates,
+    ServerSelectionScreenCoordinates,
+    GameModeScreenCoordinates,
+    MainMenuScreenCoordinates,
+)
 
-
-
-class AutoSim:
-    def __init__(self, config: Config, master, controller) -> None:
+class AutoSim(BaseTaskManager):
+    def __init__(self, loop: asyncio.AbstractEventLoop, config: Config, master, controller) -> None:
         self.config = config
+        self.hotkey = self.config.AUTOSIM_HOTKEY
+        super().__init__(loop=loop)
         self.gui = AutoSimGUI(autosim=self, config=self.config, master=master, controller=controller)
+
+    async def _task(self):
         
-        self.autosim_task = None
-        self.loop = asyncio.new_event_loop()
-        self.thread = threading.Thread(target=self.start_loop, args=(self.loop,))
-        self.thread.start()
-        self.autosim_running = False  # This will track the running state internally, not in config
-        keyboard.register_hotkey(self.config.AUTOSIM_HOTKEY, self.toggle_autosim, suppress=True)
-        print("AutoSim initialized\n")
-
-
-    def start_loop(self, loop: asyncio.AbstractEventLoop):
-        asyncio.set_event_loop(loop)
-        loop.run_forever()
+        pa.move_cursor_and_click(MainMenuScreenCoordinates.PRESS_TO_START) # Press middle button on the start screen
         
-    def start_autosim(self):
-        assert not self.autosim_running
-        self.autosim_running = True
-        self.loop.call_soon_threadsafe(self._start_autosim_coroutine)
-        print("Auto-sim started\n")
-
-    def _start_autosim_coroutine(self):
-        self.autosim_task = asyncio.create_task(self.run_auto_sim())
-
-    def stop_autosim(self):
-        if self.autosim_running:
-            self.autosim_running = False
-            if self.autosim_task is not None:
-                self.loop.call_soon_threadsafe(self._stop_autosim_coroutine)
-            print("Auto-sim stopped")
-
-    def _stop_autosim_coroutine(self):
-        self.autosim_task.cancel()
-    
-    async def run_auto_sim(self):
-        try:
-            while self.autosim_running:
-                map_number = self.gui.text_input.get()
-                await self._autosim_routine(map_number)
-        except Exception as e:
-            print(f"Error: {e}")
-            self.autosim_running = False
-
-    def toggle_autosim(self, event=None):
-        if self.autosim_running:
-            self.stop_autosim()
-        elif self.autosim_task is None or self.autosim_task.done():
-            self.start_autosim()
-
-    def get_screen_resolution(self):
-        width, height = pyautogui.size()
-        return width, height
-    
-    async def move_and_click(self, x, y, sleep_time=1):
-        screen_w, screen_h = self.get_screen_resolution()
-        # Default values are for 1920x1080 resolution, so we need to check if the resolution is different
-        if screen_w != 1920 or screen_h != 1080:
-            x *= screen_w / 1920
-            y *= screen_h / 1080
-        pyautogui.moveTo(x, y)
-        pyautogui.click()
-        await asyncio.sleep(sleep_time)
+        pa.move_cursor_and_click(GameModeScreenCoordinates.JOIN_GAME)  # Press Join Game button
         
-    async def _autosim_routine(self, map_number):
-        while self.autosim_running:
-            await self.move_and_click(934, 858)  # Press middle button on the start screen
-            await self.move_and_click(705, 523)  # Press Join Game button
-            await self.move_and_click(1670, 200)  # Click on the map search bar
-            pyautogui.write(map_number)
-            pyautogui.press('enter')
-            await asyncio.sleep(0.5)  # Wait for the map list to load
-            await self.move_and_click(930, 335)  # Click on the first map in the list
-            await self.move_and_click(930, 335)  # Click again to confirm the map selection, sometimes the first click doesn't register
-            await self.move_and_click(1700, 945, 5)  # Click Join and wait 3 seconds for the mod selection screen to load
-            await self.move_and_click(340, 930)  # Click Join
-            await asyncio.sleep(10)  # Delay to allow the Server full message to appear
-            pyautogui.press('esc')
-            await asyncio.sleep(0.5)
-            await self.move_and_click(170, 880)
-            await self.move_and_click(964, 964)
+        pa.move_cursor_and_click(ServerSelectionScreenCoordinates.SEARCH_BOX)  # Click on the map search bar
+        pyautogui.write(self.gui.text_input.get())
+        pyautogui.press('enter')
+        asyncio.sleep(0.5)  # Wait for the map list to load
+        pa.move_cursor_and_click(ServerSelectionScreenCoordinates.FIRST_SERVER)  # Click on the first map in the list
+        pa.move_cursor_and_click(ServerSelectionScreenCoordinates.FIRST_SERVER)  # Click again to confirm the map selection, sometimes the first click doesn't register
+        pa.move_cursor_and_click(ServerSelectionScreenCoordinates.JOIN)  # Click Join and wait 3 seconds for the mod selection screen to load
+        pa.move_cursor_and_click(ModsSelectionScreenCoordinates.JOIN, prev_delay=3)  # Click Join
+        await asyncio.sleep(10)  # Delay to allow the Server full message to appear
+        pyautogui.press('esc')
+        await asyncio.sleep(0.5)
+        pa.move_cursor_and_click(ServerSelectionScreenCoordinates.BACK)
+        pa.move_cursor_and_click(GameModeScreenCoordinates.BACK)
+
             
-    def destroy_loop(self) -> None:
-        print("Destroying AutoSim...")
-        self.loop.call_soon_threadsafe(self.loop.stop)
-        while self.loop.is_running():
-            time.sleep(0.1)
-        self.loop.close()
-        self.thread.join()
+    def register_key(self) -> None:
+        keyboard.register_hotkey(self.hotkey, self.toggle_task, suppress=True)
+        print(f"Registered hotkey: {self.hotkey}")
+    
+    def unregister_key(self) -> None:
+        keyboard.unregister_hotkey(self.hotkey)
+        print(f"Unregistered hotkey: {self.hotkey}")
 
 class AutoSimGUI(BaseFrame):
 
@@ -130,7 +82,7 @@ class AutoSimGUI(BaseFrame):
         map_num_entry.pack(padx=10, pady=10)
         
     def destroy_gui(self) -> None:
-        self.autosim.destroy_loop()
+        self.autosim.destroy()
         super().destroy()
         print("AutoSim destroyed")
     
