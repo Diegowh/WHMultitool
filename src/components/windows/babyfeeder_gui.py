3d7f55f12e5237fc9211192dffde1dd7bc7c1b92
diegowh <contacto.diegowh@gmail.com>
2024-05-29 10:32:48 +0200

    Add mode selection radio buttons

--- src/components/windows/babyfeeder_gui.py
+++ src/components/windows/babyfeeder_gui.py
@@ -27,6 +27,7 @@ class BabyFeederGUI(ConfigurableFrame):
         self.foods: list[str] = self.service_controller.app_config.foods
         self.toggle_key_label = None
         self.selected_food = None
+        self.mode = None
         self.init_gui()
         
     def init_gui(self):
@@ -37,15 +38,19 @@ class BabyFeederGUI(ConfigurableFrame):
         )
         title_frame.pack(side=tk.TOP, fill=tk.X)
         
-        self.item_selection_frame = ttk.Frame(self)
-        self.item_selection_frame.pack(pady=10)
-        
+        self.selection_frame = ttk.Frame(self)
+        self.selection_frame.pack(pady=10, fill=tk.X)
+
         self.create_item_selection()
-        
+        self.create_mode_selection()
+
         self.toggle_key_label = tk.Label(self, text=f"Press '{(self.config.toggle_key).upper()}' to toggle on/off", font=("Arial", 8, "italic"))
         self.toggle_key_label.pack(pady=20)
 
     def create_item_selection(self):
+        self.item_selection_frame = ttk.Frame(self.selection_frame)
+        self.item_selection_frame.grid(row=0, column=0, sticky='w', padx=20)
+
         self.selected_food = tk.StringVar(value=self.foods[0] if self.foods else None)
 
         for index, food in enumerate(self.foods):
@@ -56,8 +61,23 @@ class BabyFeederGUI(ConfigurableFrame):
                 value=food,
             )
             rb.grid(row=index, column=0, sticky='w')
-            
-    
+
+    def create_mode_selection(self):
+        self.mode = tk.StringVar(value="Single")  # Set default value to "Single"
+
+        self.mode_selection_frame = ttk.Frame(self.selection_frame)
+        self.mode_selection_frame.grid(row=0, column=1, sticky='e', padx=40)
+
+        modes = ["Single", "Bucle"]
+        for index, mode in enumerate(modes):
+            rb = ttk.Radiobutton(
+                self.mode_selection_frame, 
+                text=mode,
+                variable=self.mode,
+                value=mode,
+            )
+            rb.grid(row=index, column=0, sticky='w')
+
     def destroy_gui(self):
         self.service_controller.destroy()
         super().destroy()
